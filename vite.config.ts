4db30d5938e1ada5f9b7d50b230aab98e5fbb7db
anggafr12 <anggaferdiant11@gmail.com>
2025-09-26 19:27:27 +0700

    bismillllahhh

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
-  define: {
-    'import.meta.env.VITE_BASENAME': JSON.stringify(process.env.VITE_BASENAME || ''),
+  build: {
+    outDir: 'dist'
   }
 })
